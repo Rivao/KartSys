18ebaaa559904d1cfbd569325b09c488d444bb17
Rivao <raivokokarevics@gmail.com>
2018-01-15 01:49:48 +0200

    testing purposes

--- resources/views/karts/kart.blade.php
+++ resources/views/karts/kart.blade.php
@@ -4,15 +4,22 @@
 
 	<div class = "container text-center">
 		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+            	<div class="panel panel-default">
+		       	 	<div class="panel-heading">View Go-Kart</div>
+		        		<div class="panel-body ">
 
-			<p class="col-xs-4">{{ $kart->kart_nr }}</p>
-			<p class="col-xs-4">{{ $kart->model }}</p>
-			<p class="col-xs-4">{{ $kart->usable }}</p>
-			<p class="col-xs-4">{{ $kart->on_track }}</p>
-			<p class="col-xs-4">{{ $kart->broken }}</p>
-			<p class="col-xs-4">{{ $kart->created_at }}</p>
-			<p class="col-xs-4">{{ $kart->updated_at }}</p>
+							<p class="col-s-1 bg-info">{{ $kart->kart_nr }}</p>
+							<p class="col-xs-3 bg-info">{{ $kart->model }}</p>
+							<p class="col-xs-1 bg-info">{{ $kart->usable }}</p>
+							<p class="col-xs-1 bg-info">{{ $kart->on_track }}</p>
+							<p class="col-xs-1 bg-info">{{ $kart->broken }}</p>
+							<p class="col-xs-2 bg-info">{{ $kart->created_at }}</p>
+							<p class="col-xs-2 bg-info">{{ $kart->updated_at }}</p>
 
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
