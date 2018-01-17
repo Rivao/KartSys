89f164a94f9009b329ca8455d123dc26ccb27d1a
LieneAlka <lienealka@gmail.com>
2018-01-17 17:09:01 +0200

    Reservations.index + validation

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 	
 	<div class = "container text-center">
-		<a href = '{{ route('kartAdd') }}'>
+		<a href = '{{ route('reservAdd') }}'>
 		<button class = 'btn-block btn-primary'>Add new reservation</button>
 		</a>
 		<table id="myTable" class = "table table-bordered table-striped table-hover">
@@ -17,9 +17,35 @@
 					<th class="text-center">Time</th>
 					<th class="text-center">Length of the ride</th>
 					<th class="text-center">Number of riders</th>
+					<th class="text-center">Added</th>
+					<th class="text-center">Employee</th>
+					<th class="text-center">Edit</th>
+					<th class="text-center">Delete</th>
 				</tr>
 			</thead>
 		
+			<tbody>
+				@foreach( $reservations as $reservation)
+				
+				<tr>
+					<td>{{ $reservation->first_name }}</td>
+					<td>{{ $reservation->last_name }}</td>
+					<td>{{ $reservation->number }}</td>
+					<td>{{ $reservation->date }}</td>
+					<td>{{ $reservation->hours }} : {{ $reservation->minutes }}</td>
+					<td>{{ $reservation->length }}</td>
+					<td>{{ $reservation->numberRiders }}</td>
+
+					<td>{{ $reservation->created_at }}</td>
+					<td>{{ $reservation->employee_id }}</td>
+					<td><a >Edit</a></td>
+					<td><a >Delete</a></td>
+					
+				</tr>
+
+				@endforeach
+			</tbody>
+
 		</table>
 	</div>
 
